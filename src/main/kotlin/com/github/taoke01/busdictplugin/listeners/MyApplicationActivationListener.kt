be9986bbe2e4b8274afa62070e1f8ae2bd880834
GitHub Action <action@github.com>
2024-04-06 07:46:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/taoke01/busdictplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/taoke01/busdictplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.taoke01.busdictplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
